8a86b855b75f406962b6df0456a93e7de8bf1e2d
Yonathan Nieles <yonathannieles011@gmail.com>
2025-11-29 18:38:17 -0400

    terminada

--- src/validators/products.validators.js
+++ src/validators/products.validators.js
@@ -3,7 +3,6 @@ import { body, param, validationResult } from 'express-validator';
 import { CategoriesServices } from '../services/categories.service.js';
 import { AreasServices } from '../services/areas.service.js';
 
-// Valida que el campo nombre no este vacio
 const validatorName = async (req, res, next) => {
     await body('name').notEmpty().withMessage('El campo nombre no puede estar vacío').run(req);
 
@@ -17,7 +16,6 @@ const validatorName = async (req, res, next) => {
     next();
 };
 
-// Valida que el campo id sea un número entero
 const validatorId = async (req, res, next) => {
     await param('id').isInt().withMessage('El campo id debe ser un número entero').run(req);
 
@@ -31,7 +29,7 @@ const validatorId = async (req, res, next) => {
     next();
 };
 
-// Valida que el campo price sea un número
+
 const validatorPrice = async (req, res, next) => {
     await body('price')
         .isFloat({ min: 0 }).withMessage('El campo precio debe ser un número válido mayor o igual a 0')
@@ -47,7 +45,7 @@ const validatorPrice = async (req, res, next) => {
     next();
 };
 
-// Valida que el campo quantity sea un número entero
+
 const validatorQuantity = async (req, res, next) => {
     await body('quantity')
         .isInt({ min: 0 }).withMessage('El campo cantidad debe ser un número entero mayor o igual a 0')
@@ -63,7 +61,7 @@ const validatorQuantity = async (req, res, next) => {
     next();
 };
 
-// Valida que el campo category_id sea un número entero
+
 const validatorCategoryId = async (req, res, next) => {
     await body('category_id')
         .isInt().withMessage('El campo category_id debe ser un número entero válido')
@@ -79,7 +77,7 @@ const validatorCategoryId = async (req, res, next) => {
     next();
 };
 
-// Valida que el campo area_id sea un número entero
+
 const validatorAreaId = async (req, res, next) => {
     await body('area_id')
         .isInt().withMessage('El campo area_id debe ser un número entero válido')
@@ -123,7 +121,7 @@ const validatorAreaExist = async (req, res, next) => {
     next();
 }
 
-// Valida que exista un registro con el id que se le pasa por parámetro
+
 const validatorIdExist = async (req, res, next) => {
     const { id } = req.params;
 
@@ -138,7 +136,7 @@ const validatorIdExist = async (req, res, next) => {
     next();
 }
 
-// Valida que no exista un producto con el mismo nombre en la misma área
+
 const validatorNameAreaExist = async (req, res, next) => {
     const { name, area_id } = req.body;
 
